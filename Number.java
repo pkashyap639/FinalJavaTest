8ec668d77c02249d03b31fa1a4509e8353502dd5
pkashyap639 <138331205+pkashyap639@users.noreply.github.com>
2023-12-09 10:50:10 -0500

    Number Class

---
+++ Number.java
@@ -0,0 +1,20 @@
+public class Number extends ArithmeticExpression{
+    private int value;
+
+    public Number(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    @Override
+    public void evaluate() {
+
+    }
+}
